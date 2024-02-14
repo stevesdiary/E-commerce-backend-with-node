e71828a72ddfb3c344e36107c0d822bdf7d10f7d
stevesdiary <stephenoyeyemi@gmail.com>
2024-02-14 09:31:41 +0100

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,18 +1,21 @@
-const Sequelize = require("sequelize");
-const path = require('path');
-const dbConfig = require('../config/dbConfig');
-const fs = require('fs');
-const basename = path.basename(__filename);
+'use strict';
 
-const sequelize = new Sequelize(
-  dbConfig.DATABASE,
-  dbConfig.USER,
-  dbConfig.PASSWORD,
-  {
-    dialect: dbConfig.DIALECT || 'mysql',
-    host: dbConfig.HOST,
-  });
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const process = require('process');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
 fs
   .readdirSync(__dirname)
   .filter(file => {
@@ -33,9 +36,8 @@ Object.keys(db).forEach(modelName => {
     db[modelName].associate(db);
   }
 });
-db.sequelize = sequelize;
-db.models = {};
 
-db.models.models = require('./user')(sequelize, Sequelize.DataTypes);
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
 module.exports = db;
