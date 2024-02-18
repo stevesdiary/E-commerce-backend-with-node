a3ad7987b665676485e50d377db6af4720d0f30c
stevesdiary <stephenoyeyemi@gmail.com>
2024-02-18 19:32:21 +0100

    Update registerController.js

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -6,17 +6,17 @@ const saltRounds = bcrypt.genSaltSync(11);
 const registerController = {
   registerUser: async (req, res) => {
     try{
-      const { first_name, last_name, phone_number, gender, email, password, confirmPassword, type } = req.body;
+      const { first_name, last_name, phone_number, gender, email, password, confirm_password, type } = req.body;
       const id = uuidv4();
       const userExists = await User.findOne({ where: { email } });
 
       if (userExists) {
         return res.status(409).json({ Message: `User ${first_name} already exists, you can login with your password.` });
       }
-      if (password !== confirmPassword) {
+      if (password !== confirm_password) {
         res.status(409).send({message: 'Password do not match, check and try again.'})
       }
-      if(password == confirmPassword) {
+      if(password == confirm_password) {
         const hashedPassword = await bcrypt.hash(password, saltRounds);
       
       const userRecord = await User.create({ id, first_name, last_name, phone_number, gender, email, password: hashedPassword, type });
