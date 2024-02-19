c1fbeb3d5fc63050ab1335259fa020a87c2b513c
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2024-02-19 11:48:32 +0100

    Update register.js

--- routes/register.js
+++ routes/register.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const registerController = require('../controllers/registerController.js');
 
-router.post('/signup', registerController.registerUser);
+router.post('/register', registerController.registerUser);
 
 module.exports = router;
