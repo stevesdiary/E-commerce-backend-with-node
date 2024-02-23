5f6f95f33dd83c784bbf1c08d88d83954db4e569
Stephen Oyeyemi <stephenoyeyemi@Stephens-MacBook-Pro-2.local>
2024-02-23 01:04:05 +0100

    Create image.js

---
+++ models/image.js
@@ -0,0 +1,40 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Image extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Image.belongsToMany(models.Product, { foreignKey: 'image_id', as: 'image' });
+    }
+  }
+  Image.init({
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey:true,
+      defaultValue: UUID
+  },
+    product_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image_url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, {
+    sequelize,
+    tableName: 'Images',
+    modelName: 'Image',
+    freezeTableName: true,
+    paranoid: false
+  });
+  return Image;
+};
