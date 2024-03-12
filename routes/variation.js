ceb69b4d76b48736f893e0c3e28879309aba0fbb
stevesdiary <94493468+stevesdiary@users.noreply.github.com>
2024-03-13 00:05:21 +0100

    Update variation.js

--- routes/variation.js
+++ routes/variation.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const verifyType = require('../middlewares/verifyUserType');
-const featureController = require('../controllers/variationController');
+const variationController = require('../controllers/variationController');
 const { authentication } = require('../middlewares/authentication');
 
-router.get('/allvariations', featureController.findAllVariations); //authentication, verifyType(['admin']),
+router.get('/allvariations', variationController.findAllVariations); //authentication, verifyType(['admin']),
 
-router.get('/feature/:id', featureController.findOne); // authentication, verifyType(['admin']),
+router.get('/feature/:id', variationController.findOne); // authentication, verifyType(['admin']),
 
-router.put('/feature/:id', featureController.updateVariation); //authentication, verifyType(['admin'])
+router.put('/feature/:id', variationController.updateVariation); //authentication, verifyType(['admin'])
 
-router.delete('/feature/:id', featureController.deleteVariation);
+router.delete('/feature/:id', variationController.deleteVariation);
 
 module.exports = router;
