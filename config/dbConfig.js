d0be032b1e211cbf910d7619f0318abb2aa9ac7c
stevesdiary <94493468+stevesdiary@users.noreply.github.com>
2024-03-13 13:50:14 +0100

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(
-  process.env.DATABASE,
-  process.env.USERNAME,
-  process.env.PASSWORD,
+  database = process.env.DATABASE,
+  username = process.env.USERNAME,
+  password = process.env.PASSWORD,
   {
     dialect: process.env.DIALECT || "mysql",
     host: process.env.HOST,
   }
 );
-console.log("ENV VALUES", process.env.HOST)
+
 try {
   sequelize.authenticate();
   console.log("Connection has been established.");
@@ -18,26 +18,3 @@ try {
 }
 
 module.exports = sequelize;
-
-// require("dotenv").config();
-// const Sequelize = require("sequelize");
-// const sequelize = new Sequelize(
-//   process.env.DATABASE,
-//   process.env.USERNAME,
-//   process.env.PASSWORD,
-//   {
-//     dialect: process.env.DIALECT || "mysql",
-//     host: process.env.HOST,
-//   }
-// );
-
-// // Authenticating and logging success message
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log("Connection to the database has been established successfully.");
-//   })
-//   .catch((error) => {
-//     console.error("Unable to connect to the database:", error);
-//   });
-// module.exports = sequelize;
