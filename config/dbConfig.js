3606454b5510f018e7855f927c4034380f920c25
stevesdiary <94493468+stevesdiary@users.noreply.github.com>
2024-03-19 22:38:57 +0100

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,20 +1,8 @@
-require("dotenv").config();
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize(
-  database = process.env.DATABASE,
-  username = process.env.USERNAME,
-  password = process.env.PASSWORD,
-  {
-    dialect: process.env.DIALECT || "mysql",
-    host: process.env.HOST,
-  }
-);
-
-try {
-  sequelize.authenticate();
-  console.log("Connection has been established.");
-} catch (error) {
-  console.log("Unable to connect to the database", error);
-}
-
-module.exports = sequelize;
+module.exports = {
+  HOST: process.env.HOST,
+  USERNAME: process.env.USERNAME,
+  PORT: process.env.PORT,
+  PASSWORD: process.env.PASSWORD,
+  DATABASE: process.env.DATABASE,
+  DIALECT: process.env.DIALECT
+};
