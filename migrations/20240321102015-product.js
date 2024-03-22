7545c13d09b2ed9f54d4108e0e01665af4b2396c
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2024-03-22 23:30:10 +0100

    Create 20240321102015-product.js

---
+++ migrations/20240321102015-product.js
@@ -0,0 +1,61 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Products', {
+      id: {
+        type: Sequelize.UUID,
+        allowNull: false
+      },
+      variation_id: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      image_id: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      category: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      price: {
+        type: Sequelize.FLOAT,
+      allowNull: false
+      },
+      discount: {
+        type: Sequelize.FLOAT,
+      allowNull: false
+      },
+      in_stock: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Products');
+  }
+};
