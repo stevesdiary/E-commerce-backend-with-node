9c836de1f1a9b48ea4001745d2b03753012b72ee
steve-aella <94493468+stevesdiary@users.noreply.github.com>
2024-04-02 10:11:10 +0100

    Find product by size.

--- routes/product.js
+++ routes/product.js
@@ -9,6 +9,7 @@ router.post('/createproduct', productController.createProduct);
 router.get('/allproduct', productController.findAllProducts); //authentication, verifyType(['admin']),
 
 router.get('/product/:id', productController.findOne); // authentication, verifyType(['admin']),
+router.get('/product', productController.findBySize); // authentication, verifyType(['admin']),
 
 router.put('/product/:id', productController.updateProduct); //authentication, verifyType(['admin'])
 
