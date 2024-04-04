43e5a3a7e22891606e8776a44e374f8692a0db19
steve-aella <94493468+stevesdiary@users.noreply.github.com>
2024-04-04 11:30:32 +0100

    Create 20240404091810-User.js

---
+++ migrations/20240404091810-User.js
@@ -0,0 +1,67 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      user_id: {
+        type: Sequelize.UUID,
+        allowNull: false
+      },
+      first_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      last_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      gender: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      phone_number: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      billing_address: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      shipping_address: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      type: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: null
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
