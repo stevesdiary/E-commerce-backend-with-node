d2b4d19865367e08c53c1c8c81798acf81d6c0f3
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2024-04-06 01:35:05 +0100

    Update variation.js

--- routes/variation.js
+++ routes/variation.js
@@ -7,9 +7,8 @@ const { authentication } = require('../middlewares/authentication');
 router.get('/allvariations', variationController.findAllVariations); //authentication, verifyType(['admin']),
 
 router.get('/variations', variationController.findBySize); // authentication, verifyType(['admin']),
-// router.get('/:id', variationController.findOne); // authentication, verifyType(['admin']),
 
-router.put('/feature/:id', variationController.updateVariation); //authentication, verifyType(['admin'])
+router.put('/variation/:variation_id', variationController.updateVariation); //authentication, verifyType(['admin'])
 
 router.delete('/feature/:id', variationController.deleteVariation);
 
