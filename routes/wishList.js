a4f9b2f63d036c7a3b0b8ff51cb6d8be24451aff
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2024-04-06 01:35:18 +0100

    Update wishList.js

--- routes/wishList.js
+++ routes/wishList.js
@@ -3,13 +3,10 @@ const router = express.Router();
 const verifyType = require('../middlewares/verifyUserType');
 const wishListController = require('../controllers/wishListController');
 const { authentication } = require('../middlewares/authentication');
-console.log("Reaching here...");
 
 router.post('/wish', wishListController.createWishList);
 
 router.get('/wishlists', wishListController.findAllWishes); //authentication, verifyType(['admin']),
-
-router.put('/wishlist', wishListController.updateWishList); //authentication, verifyType(['admin'])
 
 router.delete('/wishlist/:wish_id', wishListController.deleteWish);
 
