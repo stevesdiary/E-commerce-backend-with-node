fbc70248c91656097c7c1a7b3a7325b9336a1ca6
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2024-04-06 01:42:47 +0100

    Update variationController.js

--- controllers/variationController.js
+++ controllers/variationController.js
@@ -9,7 +9,7 @@ const variationController = {
         }
       });
       // console.log('Records found', variations)
-      return res.status(200).send({ Message: 'Records found', variations})
+      return res.status(200).send({ Message: 'Records found', Result: variations})
     }catch(err){
       console.log('An error occoured!', err);
       return res.send({message: 'Error showed up', Error: err.message})
@@ -19,7 +19,6 @@ const variationController = {
   findBySize: async (req, res) => {
     try{
       const size = req.query.size;
-      console.log(size);
       const variation = await Variation.findAll({
         where: {size}, 
         attributes: {
@@ -36,32 +35,27 @@ const variationController = {
 
   deleteVariation: async (req, res ) => {
     try{
-      const id = req.params.id;
-      console.log("IDDDD", id)
-      const variation = await Variation.destroy({where: {id}})
-      // console.log(user)
-      if (variation == 1 ){
-        return res.send({message: `User with id ${id} has been deleted successfully!`})
-      }
-      if(user == 0){
-        return res.send({message: `User ${id} does not exist or is deleted in the database`})
-      }
+      const variation_id = req.params.variation_id;
+      const variation = await Variation.destroy({where: {variation_id}});
+      const message = variation === 1
+      ? `Wish with id: ${variation_id} has been deleted successfully!`
+      : `User ${variation_id} does not exist or is deleted in the database`;
+      return res.send({ message });
     }catch(err){
       return res.status(500).send({message: 'Error occoured', Error: err.message})
     }
   },
 
   updateVariation: async (req, res) => {
-  try{
-    const id = req.params.id;
-    const {colour, size, pattern} = req.body;
-    const updateVariation = await User.update({name, address, email, phone_number, type }, {where: {id}});
-    // console.log(updateVariation);
-    return res.status(200).send({ message: 'Record Updated' });
-  } catch (err) {
-    console.log(err.message);
-    return res.status(500).send({message: 'Error occoured', Error: err.message})
-  }
+    try{
+      const variation_id = req.params.variation_id;
+      const {colour, size, style} = req.query;
+      const updateVariation = await User.update({colour, size, style }, {where: {variation_id}});
+      return res.status(200).send({ message: 'Record Updated', Result: updateVariation });
+    } catch (err) {
+      console.log(err.message);
+      return res.status(500).send({message: 'Error occoured', Error: err.message})
+    }
   }
 }
 
