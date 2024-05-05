5a3019d363540109d3b86f34fae665041c1f79a0
stevesdiary <94493468+stevesdiary@users.noreply.github.com>
2024-05-05 20:24:44 +0100

    Update media.js

--- routes/media.js
+++ routes/media.js
@@ -5,6 +5,6 @@ const mediaController = require('../controllers/mediaController');
 const { authentication } = require('../middlewares/authentication');
 
 // router.post('/upload', mediaController.media);
-router.post("/upload", mediaController.uploadMedia, mediaController.handleUpload);
+// router.post("/uploadbulk", mediaController.uploadMedia, mediaController.handleUpload);
 
 module.exports = router;
