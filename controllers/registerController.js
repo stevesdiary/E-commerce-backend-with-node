ec852dfb8caa15d0b66cb96ac47245e2a7f482c3
steve-aella <stephenoyeyemi@gmail.com>
2024-09-08 00:02:09 +0100

    Update registerController.js

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -6,35 +6,7 @@ const registerController = {
   registerUser: async (req, res) => {
     try{
       const { first_name, last_name, phone_number, shipping_address, billing_address, gender, email, password, confirm_password, type } = req.body;
-      const user_id = uuidv4();
-      const userExists = await User.findOne({ where: { email } });
-      const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&^])[A-Za-z\d@.#$!%*?&^]{8,15}$/;
-      if (userExists) {
-        return res.status(409).json({ Message: `User ${first_name} already exists, you can login with your password.` });
-      }
-      if ( passwordRegex.test(password) == false ) {
-        return res.status(403).send({
-          Message: 'Password must be at least 8 characters long, maximum of 15 characters and include at least one lowercase letter, one uppercase letter, one numeric digit, and one special character.'
-        })
-      }
-      if (password !== confirm_password) {
-        res.status(409).send({message: 'Password do not match, check and try again.'})
-      }
-      if(passwordRegex.test(password) == true && password == confirm_password) {
-        const hashedPassword = await bcrypt.hash(password, saltRounds);
-        const userRecord = await User.create({ user_id, first_name, last_name, phone_number, shipping_address, billing_address, gender, email, password: hashedPassword, type });
-        if (userRecord) {
-          const sanitizedUser = await User.findByPk(userRecord.user_id, {
-            attributes: { exclude: ['password'] },
-          }); 
-          return res.status(201).json({ Message: `User ${first_name} created successfully`, User: sanitizedUser });
-        }
-      }
-    }catch(err) {
-      console.log(err)
-      return res.status(500).send({message: "An error occoured!", Error: ( err.type, err.message ) });
     }
-  }
 }
 
 module.exports = registerController;
