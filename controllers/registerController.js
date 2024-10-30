45b2aff171a22d44ec84634ee3f1bf4b9d4a51cc
stevesdiary <stephenoyeyemi@gmail.com>
2024-10-30 11:39:31 +0100

    Update registerController.js

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -7,6 +7,11 @@ const registerController = {
     try{
       const { first_name, last_name, phone_number, shipping_address, billing_address, gender, email, password, confirm_password, type } = req.body;
     }
+    catch(err){
+      console.log(err);
+      return err
+    }
+  }
 }
 
 module.exports = registerController;
